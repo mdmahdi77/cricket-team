1e75b219fc30ebf559e53d840c456d7a939ee1c6
mehedi hasan <mehedihasanibnc@gmail.com>
2021-03-01 07:50:20 +0600

    something

--- src/components/Selected/Selected.js
+++ src/components/Selected/Selected.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Selected.css';
 
 const Selected = (props) => {
-    const {image, fname, lname} = props.select;
+    const {image, fname, lname, price} = props.select;
     return (
         <div className="selected">
             <img src={image} alt=""/>
-            <h2>{fname + " " + lname}</h2>
+            <h2>{fname + " " + lname}({price})</h2>
         </div>
     );
 };
